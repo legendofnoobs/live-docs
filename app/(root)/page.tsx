76c1971d7d4fa92dbb792ebcfc3d2ffad9682201
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-04-21 21:39:20 +0200

    made the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,50 @@
-import { Button } from '@/components/ui/button'
+import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
 import React from 'react'
 
-const Home = () => {
+import doc from "@/public/assets/icons/doc.svg"
+import AddDocumentBtn from '@/components/AddDocumentBtn'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+const Home = async () => {
+  const clerkUser = await currentUser()
+  if (!clerkUser) redirect('/sign-in');
+
+  const documnents = []
   return (
-    <div>
-      <Button>click me</Button>
-    </div>
+    <main className='home-container'>
+      <Header className='sticky left-0 top-0'>
+        <div className='flex items-center gap-2 lg:gap-4'>
+          Notifications
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {
+        documnents.length > 0 ? (
+          <div></div>
+        ) : (
+          <div className='document-list-empty'>
+            <Image
+              src={doc}
+              alt='doc'
+              width={40}
+              height={40}
+              className='mx-auto'
+            />
+
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+        )
+      }
+    </main>
   )
 }
 
